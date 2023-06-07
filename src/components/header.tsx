860573c785570a1b7279b9c1a4acfbef60e99ccc
Herbie Vine <vineherbie@gmail.com>
2023-06-07 22:53:06 +0200

    added whitelisted emails in middleware + error message

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ type HeaderProps = {
 };
 
 export default function Header({ session }: HeaderProps) {
-  const { push } = useRouter();
+  const { push, back } = useRouter();
   const path = usePathname();
   const query = useSearchParams();
 
@@ -21,12 +21,9 @@ export default function Header({ session }: HeaderProps) {
   return (
     <header className="w-full flex justify-between items-center relative">
       {path !== "/" ? (
-        <button
-          className="flex space-x-2 items-center"
-          onClick={() => push("/")}
-        >
+        <button className="flex space-x-2 items-center" onClick={back}>
           <ChevronIcon className="w-3.5 h-3.5 fill-blue-400 rotate-90" />
-          <span className="font-bold text-blue-400">Music</span>
+          <span className="font-bold text-blue-400">Back</span>
         </button>
       ) : (
         <h1 className="font-black text-3xl">Music</h1>
