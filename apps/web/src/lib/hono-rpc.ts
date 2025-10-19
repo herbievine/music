2ca34f4fe376c5b54601c83b9f3fadfa31211ee2
Herbie Vine <me@herbievine.com>
2025-10-19 14:28:31 +0200

    fix: process -> import.meta

--- apps/web/src/lib/hono-rpc.ts
+++ apps/web/src/lib/hono-rpc.ts
@@ -1,11 +1,11 @@
 import type { AppType } from "@music/api";
 import { hc } from "hono/client";
 
-if (!process.env.VITE_API_URL) {
+if (!import.meta.env.VITE_API_URL) {
 	throw new Error("VITE_API_URL not set");
 }
 
-export const client = hc<AppType>(process.env.VITE_API_URL, {
+export const client = hc<AppType>(import.meta.env.VITE_API_URL, {
 	init: {
 		credentials: "include",
 	},
