e1f4fd2995e2b527871da1521ae97b76581e2079
Herbie Vine <me@herbievine.com>
2025-10-19 14:23:45 +0200

    chore: add api url env

--- apps/web/src/lib/hono-rpc.ts
+++ apps/web/src/lib/hono-rpc.ts
@@ -1,7 +1,11 @@
 import type { AppType } from "@music/api";
 import { hc } from "hono/client";
 
-export const client = hc<AppType>("http://localhost:3000", {
+if (!process.env.VITE_API_URL) {
+	throw new Error("VITE_API_URL not set");
+}
+
+export const client = hc<AppType>(process.env.VITE_API_URL, {
 	init: {
 		credentials: "include",
 	},
